507208a7e07774f26e4a0cb3d5fae8830a1088c6
julio.dias <julio.dias@DT-ECO-24>
2020-11-11 13:37:48 -0300

    Add project files.

---
+++ ApnCore_CrudExemplo/Models/FuncionarioDAL.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ApnCore_CrudExemplo.Models
+{
+    public class FuncionarioDAL : IFuncionarioDAL
+    {
+        string connectionString = @"Password=millennium_qas3;Persist Security Info=True;User ID=millennium_qas3;Initial Catalog=millennium_qas3;Data Source=172.26.225.252";
+        //Password=millennium_qas3;Persist Security Info=True;User ID=millennium_qas3;Initial Catalog=millennium_qas3;Data Source=172.26.225.252
+        public IEnumerable<Funcionario> GetAllFuncionarios()
+        {
+            List<Funcionario> lstfuncionario = new List<Funcionario>();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT FuncionarioId, Nome,Cidade, Departamento,Sexo from dbo.crud_Funcionarios", con);
+                cmd.CommandType = CommandType.Text;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Funcionario funcionario = new Funcionario();
+                    funcionario.FuncionarioId = Convert.ToInt32(rdr["FuncionarioId"]);
+                    funcionario.Nome = rdr["Nome"].ToString();
+                    funcionario.Cidade = rdr["Cidade"].ToString();
+                    funcionario.Departamento = rdr["Departamento"].ToString();
+                    funcionario.Sexo = rdr["Sexo"].ToString();
+                    lstfuncionario.Add(funcionario);
+                }
+                con.Close();
+            }
+            return lstfuncionario;
+        }
+        public void AddFuncionario(Funcionario funcionario)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string comandoSQL = "Insert into dbo.crud_Funcionarios (Nome,Cidade,Departamento,Sexo) Values(@Nome, @Cidade, @Departamento, @Sexo)";
+                SqlCommand cmd = new SqlCommand(comandoSQL, con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", funcionario.Nome);
+                cmd.Parameters.AddWithValue("@Cidade", funcionario.Cidade);
+                cmd.Parameters.AddWithValue("@Departamento", funcionario.Departamento);
+                cmd.Parameters.AddWithValue("@Sexo", funcionario.Sexo);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public void UpdateFuncionario(Funcionario funcionario)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string comandoSQL = "Update dbo.crud_Funcionarios set Nome = @Nome, Cidade = @Cidade, Departamento = @Departamento, Sexo = @Sexo where FuncionarioId = @FuncionarioId";
+                SqlCommand cmd = new SqlCommand(comandoSQL, con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@FuncionarioId", funcionario.FuncionarioId);
+                cmd.Parameters.AddWithValue("@Nome", funcionario.Nome);
+                cmd.Parameters.AddWithValue("@Cidade", funcionario.Cidade);
+                cmd.Parameters.AddWithValue("@Departamento", funcionario.Departamento);
+                cmd.Parameters.AddWithValue("@Sexo", funcionario.Sexo);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public Funcionario GetFuncionario(int? id)
+        {
+            Funcionario funcionario = new Funcionario();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM dbo.crud_Funcionarios WHERE FuncionarioId= " + id;
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    funcionario.FuncionarioId = Convert.ToInt32(rdr["FuncionarioId"]);
+                    funcionario.Nome = rdr["Nome"].ToString();
+                    funcionario.Cidade = rdr["Cidade"].ToString();
+                    funcionario.Departamento = rdr["Departamento"].ToString();
+                    funcionario.Sexo = rdr["Sexo"].ToString();
+                }
+            }
+            return funcionario;
+        }
+        public void DeleteFuncionario(int? id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string comandoSQL = "Delete from dbo.crud_Funcionarios where FuncionarioId = @FuncionarioId";
+
+                SqlCommand cmd = new SqlCommand(comandoSQL, con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@FuncionarioId", id);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        //public void ResetarId()
+        //{
+        //    using (SqlConnection con = new SqlConnection(connectionString))
+        //    {
+        //        string comandoSQL = "DBCC CHECKIDENT ('dbo.crud_Funcionarios', RESEED, 0)";
+        //        SqlCommand cmd = new SqlCommand(comandoSQL, con);
+        //        cmd.CommandType = CommandType.Text;
+        //        con.Open();
+        //        cmd.ExecuteNonQuery();
+        //        con.Close();
+        //    }
+
+        //}
+
+    }
+}
+
